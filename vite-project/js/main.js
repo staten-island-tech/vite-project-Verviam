68f9baf3272db90893dc63cefb0b77f6bba3ceec
Evan Gao <gaoevan37@gmail.com>
2023-11-17 09:22:01 -0500

    asdf

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,6 +1,6 @@
-import '../css/style.css'
+import '../styles/style.css'
 import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
+import viteLogo from 'public/vite.svg'
 import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
@@ -22,3 +22,51 @@ document.querySelector('#app').innerHTML = `
 `
 
 setupCounter(document.querySelector('#counter'))
+
+
+// Using ViteJS, you will scaffold a single page project that will act as a store
+// front for a business. The site will render cards for products using forEach()
+// and iterate over an array in your JS files. Through User Interactions the
+// product list will change. You will use BOTH filter and map to create new
+// arrays to iterate through and display on the screen. You will also be
+// creating themes using CSS Custom Properties. Users will be able to
+// change themes as desired
+// -Use Vite to Scaffold a project
+// -Use both map and filter to create new arrays to iterate through
+// -Create two separate themes using CSS Custom Properties and allow
+// users to change theme
+
+
+// download node
+// get npm
+// npm init vite@latest
+// npm i (install in project folder and cd to project folder)
+// make css and js folder
+// in main.js do "../css/style.css"
+// <script type ="module" src="main.js"></script>
+// npm run dev
+
+// import './styles.css/ in js file
+
+// naming variables with css
+// :root {
+//    --dark-blue: #135cc5;
+//    --primary: var(var(--dark-blue))
+
+//    --h1: 7.594rem;
+//    }
+// body {
+//   background-color: var(--primary);
+//  }
+
+// h1 {
+//  font-size: var(--h1)
+// }
+
+// export {variable1, variable2}
+// import {variable1, variable2} from "./arrayOfItems"
+// import {idk} from "./Dom"
+
+// images go in public folder
+
+// npm run build --> send to dist file
