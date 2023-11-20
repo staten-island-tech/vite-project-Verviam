b4c41aef6c34a9af4327cb019619dd3a9e106db9
Evan Gao <gaoevan37@gmail.com>
2023-11-20 09:25:27 -0500

    asdfadf

---
+++ vite-project/js/dom.js
@@ -0,0 +1,5 @@
+const DOMSelectors = {
+
+}
+
+export {DOMSelectors}
--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,27 +1,9 @@
 import '../styles/style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from 'public/vite.svg'
-import { array } from './array.js'
+import { workouts } from './workouts'
+import { DOMSelectors } from './dom'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
 
-setupCounter(document.querySelector('#counter'))
+
 
 
 // Using ViteJS, you will scaffold a single page project that will act as a store
