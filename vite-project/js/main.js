47e8b0270339f2dc33f2c6a5b06218899e2f8c5c
Verviam <gaoevan37@gmail.com>
2023-11-26 14:31:24 -0500

    asdfa

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -20,20 +20,9 @@ function changeTheme() {
 }
 
 
-// Using ViteJS, you will scaffold a single page project that will act as a store
-// front for a business. The site will render cards for products using forEach()
-// and iterate over an array in your JS files. Through User Interactions the
-// product list will change. You will use BOTH filter and map to create new
-// arrays to iterate through and display on the screen. You will also be
-// creating themes using CSS Custom Properties. Users will be able to
-// change themes as desired
-// -Use Vite to Scaffold a project
 // -Use both map and filter to create new arrays to iterate through
 // -Create two separate themes using CSS Custom Properties and allow
-// users to change theme
 
-
-// get npm
 // npm init vite@latest
 // npm i (install in project folder and cd to project folder) every new os
 // npm run dev
